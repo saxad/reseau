3c94569a2faa8e8b49aaf72a970ddb40167c0200
saxad <saad_zizi@hotmail.com>
2017-02-13 10:18:20 +0100

    serveur concurrent v1.0

---
+++ serveur.py
@@ -0,0 +1,26 @@
+# -*- coding: utf8 -*-
+#serveur d’écho (version concurrente)
+import thread, time
+from socket import *
+
+Host = '127.0.0.1'
+Port = 8888
+s = socket(AF_INET, SOCK_STREAM)
+s.bind((Host, Port))
+s.listen(5)
+def now():
+	return time.ctime(time.time())
+def handleClient(connection):
+	time.sleep(5)
+	while 1:
+		data = connection.recv(1024)
+		if not data: break
+		connection.send('Echo=>%s at %s' % (data, now()))
+	connection.close()
+def dispatcher():
+	while 1:
+		connection, address = s.accept()
+		print 'Server connected by', address,
+		print 'at', now()
+		thread.start_new(handleClient, (connection,))
+dispatcher()
