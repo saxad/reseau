2899c91b8c09b9424f203f7ad3ff1ee7d7d6adfc
saxad <saad_zizi@hotmail.com>
2017-02-20 00:01:33 +0100

    serveur v1.0 pour identification

---
+++ id.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python 
+# -*- coding:utf8 -*-
+
+from socket import *
+import time
+import sqlite3
+
+############## creation d'une base de donnée 
+try:
+	bdd ="employées"
+
+	conn = sqlite3.connect(bdd)
+	cur = conn.cursor()
+	cur.execute("CREATE TABLE employee (nom TEXT, prenom TEXT,mdp TEXT)")
+	cur.execute("INSERT INTO employee(nom,prenom,mdp) VALUES('zizi','saad','azerty')")
+	cur.execute("INSERT INTO employee(nom,prenom,mdp) VALUES('idil','rida','qwerty')")
+	cur.execute("INSERT INTO employee(nom,prenom,mdp) VALUES('nanah','ayoub','raja')")
+	conn.commit()
+	cur.execute("SELECT *  FROM employee")
+	for i in cur:
+		print(i)
+except:
+	None
+
+#############################################
+
+host ='127.0.0.1'
+port = 8888
+# stdr standardiste
+stdr = socket(AF_INET,SOCK_STREAM)
+stdr.bind((host,port))
+stdr.listen(10)
+
+print("[{}] serveur en écoute sur le port[{}] \n" .format(time.ctime(),port))
+# cc = connection client , infoc = info client
+cc , infoc = stdr.accept()
+
+print("bienvenue sur le serveur de l'hopitale\nprocessus d'identifcation :\n")
+print("=================================================================\n")
+
+while True:
+	
+	cc.send(b"entrez votre nom :")
+	data = cc.recv(1024)
+	print("info recu à [{}] : {}" .format(time.ctime(), data))
+		
+
+##########    chercher si il ya un nom pareil dans la base de donnée
+	if data == "zizi":
+		cc.send("ok")
+		time.sleep(1)
+	else:
+		cc.send("nok")
+		time.sleep(1)	
+		continue
+
+####################################################################
+	cc.send("entrez votre prenom : ")
+	data = cc.recv(1024)
+	print("info recu à [{}] : {}" .format(time.ctime(), data))
+##########    chercher si il ya un nom pareil dans la base de donnée
+	if data == "saad":
+		cc.send("ok")
+		time.sleep(1)
+	else:
+		cc.send("nok")
+		time.sleep(1)	
+		continue
+
+#####################################################################
+
+	cc.send("entrez votre mpt de passe : ")
+	data = cc.recv(1024)
+	print("info recu à [{}] : {}" .format(time.ctime(), data))
+
+	if data == "azerty":
+		print(infoc,"est connecte")
+	else:
+		cc.send("nok")
+		time.sleep(1)
+		continue
