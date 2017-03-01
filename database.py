1d01b8ec76895f35f6d3c044c934141d33ec911c
saxad <saad_zizi@hotmail.com>
2017-03-01 23:33:45 +0100

    base de donné du personnel plus verificeation d'existence

---
+++ database.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+# -*- coding: utf8 -*-
+
+
+import sqlite3
+
+
+
+def creation():
+	""" creation de la table initiale avec des mdp par defaut azerty, qwerty"""
+	try:
+
+		bdd = "employees"
+		conn = sqlite3.connect(bdd)
+		conn.text_factory = str
+		cur = conn.cursor()
+		
+
+		cur.execute("CREATE TABLE employee (nom TEXT, prenom TEXT, mdp TEXT) ")
+		cur.execute("INSERT INTO employee(nom, prenom, mdp) VALUES('zizi','saad','azerty')")
+		cur.execute("INSERT INTO employee(nom, prenom, mdp) VALUES('pirieur','paul','azerty')")
+		cur.execute("INSERT INTO employee(nom, prenom, mdp) VALUES('dendekker','benjamin','azerty')")
+		cur.execute("INSERT INTO employee(nom, prenom, mdp) VALUES('hue','charly','azerty')")
+
+		conn.commit()
+		cur.execute("SELECT * FROM employee")
+		for i in cur:
+			print(i)
+	except:
+		None
+"""	
+	personne = cur.execute("SELECT *  FROM employee WHERE nom='zizi' ")
+	for p in personne:
+		print(p)
+	print("fin")
+"""
+def isintable( nomid, prenomid, mdpid,bdd="employees"):
+		
+	conn = sqlite3.connect(bdd)
+	cur = conn.cursor()
+	personne = cur.execute("SELECT *  FROM employee WHERE nom= ? and prenom= ? and mdp= ?  ",(nomid,prenomid,mdpid))
+	
+	liste = cur.fetchall()
+	
+	if  len(liste) == 0   :
+		print("mdp ou nom/prenom incorrecte")
+		return 0	
+	else:
+		print("utilisateur accepté \nVous etes connecté au serveur de l'Hopital")
+		return 1
+
+if __name__ == "__main__":
+	creation()
+	isintable('zxizi','saad','azerty',"employees")
